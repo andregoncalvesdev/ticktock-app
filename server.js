52367bbe4349d9d559d32b5e8804355da97cdc22
André Gonçalves <andre.goncalves@tekzenit.com>
2016-02-05 18:32:07 +0000

    ip

--- server.js
+++ server.js
@@ -5,11 +5,43 @@ const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
 const config = require('./webpack.config.development');
+const Curl = require( './node_modules/node-libcurl/lib/Curl' ), querystring = require( 'querystring' );
 
 const app = express();
 const compiler = webpack(config);
 
 const PORT = 3000;
+
+//get Tasks
+var curl = new Curl(),
+url  = 'https://mrticktock.com/app/api/get_tasks',
+data = { //Data to send, inputName : value
+  'email' : 'andre.goncalves@tekzenit.com',
+  'password' : 'Tekgenpt1234'
+};
+
+//You need to build the query string,
+// node has this helper function, but it's limited for real use cases (no support for array values for example)
+data = querystring.stringify( data );
+
+curl.setOpt( Curl.option.URL, url );
+curl.setOpt( Curl.option.POSTFIELDS, data );
+curl.setOpt( Curl.option.HTTPHEADER, ['User-Agent: node-libcurl/1.0'] );
+curl.setOpt( Curl.option.VERBOSE, true );
+
+console.log( querystring.stringify( data ) );
+
+curl.perform();
+
+curl.on( 'end', function( statusCode, body ) {
+
+    console.log( body );
+
+    this.close();
+});
+
+curl.on( 'error', curl.close.bind( curl ) );
+//eogetTasks
 
 app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath,
